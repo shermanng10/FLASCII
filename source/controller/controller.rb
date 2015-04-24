a056118aceac54ec5e9671d4d5b55f07b0a08f85
fishermanng & Tanya Trinkle <pair+fishermanng.ttrinkle@devbootcamp.com>
2015-04-24 16:37:42 -0400

    Adds controller:

---
+++ source/controller/controller.rb
@@ -0,0 +1,31 @@
+require_relative '../view/view'
+require_relative '../models/word_model'
+require_relative '../models/flickr_model'
+
+class Controller
+
+def start
+  View.start
+  user_input
+end
+
+def user_input
+@search_input = View.user_search_prompt
+input_check
+end
+
+def input_check
+  if !Word.word? (@search_input)
+      photo = Flickr.run(@search_input)
+      View.loading
+      View.picture_output(photo)
+      start if View.quit == "y"
+   else
+     View.search_warning
+     start
+   end
+end
+end
+
+test =  Controller.new()
+test.start
